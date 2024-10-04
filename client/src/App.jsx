590cef863467131ec4b0d72ce9434d5c3a93e02f
victoriakapelush <victoriakapelush@gmail.com>
2024-10-03 23:55:03 -0400

    Fix typo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import Header from './components/Header.jsx'
-import Main from './components/main.jsx'
+import Main from './components/Main.jsx'
 import Footer from './components/Footer.jsx'
 
 function App() {
