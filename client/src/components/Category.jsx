b7d4653a137c384585072282f8f8cdd25d998357
Victoria Kapelush <42095608+victoriakapelush@users.noreply.github.com>
2024-10-07 12:40:44 -0400

    Update Category.jsx

--- client/src/components/Category.jsx
+++ client/src/components/Category.jsx
@@ -54,7 +54,7 @@ function Category() {
         }
         const tokenWithoutBearer = token.replace("Bearer ", "");
         const response = await axios.get(
-          `http://localhost:8000/api/posts/${category}`,
+          `https://narrative-08nb.onrender.com/api/posts/${category}`,
           {
             headers: {
               Authorization: `Bearer ${tokenWithoutBearer}`,
